7ba5d2d619f938dbb454c8a4919108d5712fe35e
IlyasseDC <ilyasse.chaouki@wavestone.com>
2025-09-11 12:33:40 +0000

    test

--- postInstall.sh
+++ postInstall.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-echo "🔧 Forcing NumPy downgrade..."
-pip install "numpy<2"
+echo "🔧 Running postInstall.sh ..."
+
+# Forcer numpy 1.20.1 (compatible jpegio et torch==1.11.0+cu113)
+pip install --force-reinstall --no-cache-dir "numpy==1.20.1"
+
+# Vérification
+python - <<EOF
+import sys, numpy, torch
+print("✅ Python:", sys.version)
+print("✅ NumPy :", numpy.__version__)
+print("✅ Torch :", torch.__version__)
+EOF
